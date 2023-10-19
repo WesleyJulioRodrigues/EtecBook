24b2f151b06cf170c6dddd77dfdf90df805f6c81
Wesleyy <113564460+WesleyJulioRodrigues@users.noreply.github.com>
2023-10-18 22:25:06 -0300

    opa

---
+++ EtecBook.Server/EtecBookAPI/DataTransferObjects/LoginDto.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EtecBookAPI.DataTransferObjects;
+
+    public class LoginDto
+    {
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+
+    }
